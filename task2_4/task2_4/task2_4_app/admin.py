e148e2d3cac73581519eb6ccd1eefcfdeffd1a5f
popacopa <paushakow@gmail.com>
2025-11-07 11:26:00 +0300

    added all tables

--- task2_4/task2_4/task2_4_app/admin.py
+++ task2_4/task2_4/task2_4_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from task2_4_app.models import *
 
+#pavel 0000 - admin panel
+
+admin.site.register(Driver)
+admin.site.register(Route)
 # Register your models here.
--- task2_4/task2_4/task2_4_app/models.py
+++ task2_4/task2_4/task2_4_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=20, null=False)
+    last_name = models.CharField(max_length=20, null=False)
+    def __str__(self): return f'{self.first_name} {self.last_name}'
+
+
+class Route(models.Model):
+    city_from = models.CharField(max_length=20, null=False)
+    city_to = models.CharField(max_length=20, null=False)
+    driver_id = models.OneToOneField(Driver, on_delete=models.CASCADE, primary_key=True)
+    def __str__(self): return f'{self.city_from} - {self.city_to}'
