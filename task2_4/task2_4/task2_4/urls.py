a2e07b29ad5fcd5763429de6caeebbd0e0926e7f
popacopa <paushakow@gmail.com>
2025-11-26 16:29:05 +0300

    done

--- task2_4/task2_4/task2_4/urls.py
+++ task2_4/task2_4/task2_4/urls.py
@@ -5,5 +5,10 @@ from task2_4_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Index.as_view())
+    path('', Index.as_view()),
+    path('addItem/<str:item>/', Form.as_view()),
+    path('delete/<str:item>/<int:id>', Delete.as_view()),
+    path('delete/<str:item>/<str:id>', Delete.as_view()),
+    path('update/<str:item>/<int:id>', Update.as_view()),
+    path('update/<str:item>/<str:id>', Update.as_view())
 ]
--- task2_4/task2_4/task2_4_app/views.py
+++ task2_4/task2_4/task2_4_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
 from task2_4_app.models import Route, Driver
@@ -16,3 +16,71 @@ class Index(View):
                    "drivers" : drivers}
         
         return HttpResponse(render(template_name='index.html', request=reguest, context=context))
+    
+
+class Form(View):
+    def get(self, request, item):
+        drivers = Driver.objects.all()
+        context = {
+            'item': item,
+            'drivers': drivers,
+            'is_update': False,
+        }
+        return HttpResponse(render(template_name='form.html', request=request, context=context))
+    
+    def post(self, request, item):
+        if item == 'driver':
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            age = request.POST.get('age')
+            stage = request.POST.get('stage')
+            Driver.objects.create(first_name=first_name, last_name=last_name, age=age, stage=stage)
+        elif item == 'route':
+            city_from = request.POST.get('city_from')
+            city_to = request.POST.get('city_to')
+            driver_id = request.POST.get('driver_id')
+            driver = Driver.objects.get(id=driver_id)
+            try:
+                exiting = Route.objects.get(driver_id=driver)
+            except Route.DoesNotExist:
+                Route.objects.create(city_from=city_from, city_to=city_to, driver_id=driver)
+        return redirect('/')
+    
+class Delete(View):
+    def get(self, request, id, item):
+        if item == "driver":
+            Driver.objects.get(id=id).delete()
+        if item == "route":
+            id = id.split(' ')
+            driver = Driver.objects.get(first_name=id[0], last_name=id[1])
+            Route.objects.get(driver_id=driver).delete()
+        return redirect('/')
+    
+
+
+class Update(View):
+    def get(self, request, item, id):
+        drivers = Driver.objects.all()
+        context = {
+            'item': item,
+            'drivers': drivers,
+            'is_update': True,
+        }
+        return HttpResponse(render(template_name='form.html', request=request, context=context))
+    def post(self, request, item, id):
+        if item == 'driver':
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            age = request.POST.get('age')
+            stage = request.POST.get('stage')
+            driver = Driver.objects.filter(id = id)
+            driver.update(first_name=first_name, last_name=last_name, age=age, stage=stage)
+        elif item == 'route':
+            city_from = request.POST.get('city_from')
+            city_to = request.POST.get('city_to')
+            driver_id = request.POST.get('driver_id')
+            id = id.split(' ')
+            driver = Driver.objects.get(first_name=id[0], last_name=id[1])
+            exiting = Route.objects.get(driver_id=driver)
+            Route.objects.filter(driver_id=driver).update(city_from=city_from, city_to=city_to, driver_id=driver_id)
+        return redirect('/')
